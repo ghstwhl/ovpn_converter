25473a3ea75f168ca59964e38ee24999a2164b24
Ghostwheel <merlin@ghostwheel.com>
2024-09-05 15:42:28 +1200

    Initial commit
    Tested locally but converting a new user config and importing into a VPN client.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ovpn_converter
-Tool for converting OpenVPN configs from one format to another.
+Tool for converting OpenVPN configs from visz to ovpn.
+
+This quick and dirty tool will convert the visz format client config bundle
+exported from the Clinet Export menu in OpnSense into an ovpn config that
+has all the required keys and certs embedded in PEM format.  I found this
+was the best format for importing the config into the OpenVPN client on
+my phone.
+
+If this tool helps you, awesome.
+
+I don't have any current plans to support the other export formats.  This
+works, and that's good enough for me.
+
+If you have an issue with it, please feel free to open an issue on this repo.
+
+Copyright 2024 - Chris Knight
---
+++ ovpn_converter.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import io
+import os
+import tarfile
+import zipfile
+import argparse
+import re
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization.pkcs12 import load_key_and_certificates
+
+def cli():
+    parser = argparse.ArgumentParser(description='Convert .visz files to .ovpn')
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output')
+    return parser.parse_args()
+
+def visz_decode(file_path):
+    """Decode a .visz file into a dict of components.
+
+    Args:
+        file_path (str): File path for the exported visz config bundle
+    """
+    with tarfile.open(file_path) as zipped:
+        data = {}
+        for filepath, fileinfo in zip(zipped.getnames(), zipped.getmembers()):
+            if not fileinfo.isfile():
+                continue
+            filename = filepath.split(os.path.sep)[-1]
+            file_extension = re.fullmatch(r"^.*\.([^\.]+)$", filename).group(1)
+            if 'conf' == file_extension:
+                data['config'] = zipped.extractfile(filepath).read().decode('utf8').split('\n')
+            elif 'key' == file_extension:
+                data['tls_key'] = zipped.extractfile(filepath).read().decode('utf8').split('\n')
+            elif 'p12' == file_extension:
+                data['cert_bundle'] = {}
+                private_key, certificate, additional_certificates = load_key_and_certificates(
+                    zipped.extractfile(filepath).read(), None)
+
+                data['cert_bundle']['key'] = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption()
+                    ).decode('utf8').split('\n')
+
+                data['cert_bundle']['cert'] = certificate.public_bytes(encoding=serialization.Encoding.PEM).decode('utf8').split('\n')
+
+                data['cert_bundle']['ca'] = []
+                for cert in additional_certificates:
+                    pem = cert.public_bytes(encoding=serialization.Encoding.PEM).decode('utf8').split('\n')
+                    for line in pem:
+                       data['cert_bundle']['ca'].append(line)
+            else:
+                data[filename] = zipped.extractfile(filepath).read()
+        return data
+
+def create_ovpn_text_bundle(config):
+    """Convert the decoded visz config bundle into an in-line ovpn config
+
+    Args:
+        config (dict): dict of the config returned by visz_decode()
+    """
+
+    if 'config' in config:
+        config_bundle = ['#-- Converted from OpnSense export with https://github.com/ghstwhl/ovpn_converter --#']
+        for config_line in config['config']:
+            if 'tls-crypt' in config_line:
+                config_bundle.append('<tls-crypt>')
+                for key_line in config['tls_key']:
+                   config_bundle.append(key_line) 
+                config_bundle.append('</tls-crypt>')
+            elif 'pkcs12' in config_line:
+                for block_name in config['cert_bundle']:
+                    config_bundle.append(f"<{block_name}>")
+                    for cert_line in config['cert_bundle'][block_name]:
+                        config_bundle.append(cert_line)
+                    config_bundle.append(f"</{block_name}>")
+            elif 'Config Auto Generated for Viscosity' in config_line:
+                pass
+            else:
+                config_bundle.append(config_line) 
+
+        return config_bundle
+            
+if __name__ == '__main__':
+    args = cli()
+    results = re.fullmatch(r"^(.*/|)([^\/]*)\.(visz)$", args.input)
+    if results is not None:
+        if 'visz' == results.group(len(results.groups())):
+            config = visz_decode(args.input)
+            config_bundle = create_ovpn_text_bundle(config)
+            if args.output is not None:
+                output_file = args.output
+            else:
+                output_file = ""
+                for n in range(1,len(results.groups())):
+                    output_file += results.group(n)
+                output_file += ".ovpn"
+            with io.open(output_file, 'w') as out:
+                out.write(str('\n'.join(config_bundle)))
+    else:
+        results = re.fullmatch(r"^.*\.([^\.]+)$", args.input)
+        print(f"{results.group(len(results.groups()))} is not a supported file type.")
